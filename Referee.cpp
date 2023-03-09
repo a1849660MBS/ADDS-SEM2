b6ee6965deb854c5ffbfd3a27516df43fad19c9e
Marwan Salih <a1849660@adelaide.edu.au>
2023-03-09 10:48:17 +1030

    updtaes

--- Referee.cpp
+++ Referee.cpp
@@ -15,21 +15,13 @@ Player *Referee::refGame(Player *player1, Player *player2)
     Move *move1 = player1->makeMove();
     Move *move2 = player2->makeMove();
 
-    string player1_move = move1->getName();
-    string player2_move = move2->getName();
-
-    if (move1->getName() == move2->getName())
-    {
-        return nullptr;
-    }
-    if (std::find(move2->beatingMoves.begin(), move2->beatingMoves.end(), move1->getName()) == move2->beatingMoves.end())
-    {
-        return player1;
-    }
-    if (std::find(move2->beatingMoves.begin(), move2->beatingMoves.end(), move1->getName()) != move2->beatingMoves.end())
-    {
-        return player2;
-    }
+   if(move1 == move2){
+     return nullptr;
+   }else if(move1->getName() == "Paper"){
+    return player1;
+   } else if(move1->getName() == "Scissors"){
+    return player2;
+   }
 
     return nullptr;
 }
