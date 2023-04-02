e612d86c6368ac3d0b8c3030cdcab6b7d04b29dc
Marwan Salih <a1849660@adelaide.edu.au>
2023-04-02 20:46:06 +0930

    v12

--- Finder.cpp
+++ Finder.cpp
@@ -3,34 +3,51 @@
 using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
-    int m = s1.size();
-    int n = s2.size();
-    if (n > m) {
+    
+    if (s2.size() > s1.size()) {
         return result;
     }
     // Create prefix table
-    vector<int> pi(n);
-    for (int i = 0, j = 0; i < n; i++) {
-        while (j > 0 && (s2[i] != s2[j])) {
-            j = pi[j-1];
+    vector<int> pi(s2.size());
+    for (int i = 0, j = 0; i < s2.size(); i++) {
+        while (j > 0 && s2[i] != s2[j]) {
+        j = pi[j-1];
         }
         if (s2[i] == s2[j]) {
             j++;
         }
         pi[i] = j;
     }
+
     // Search for s2 in s1
-    for (int i = 0, j = 0; i < m; i++) {
-        while (j > 0 && (s1[i] != s2[j])) {
+
+    for (int i = 0, j = 0; i < s1.size(); i++) {
+        while (j > 0 && s1[i] != s2[j]) {
             j = pi[j-1];
         }
         if (s1[i] == s2[j]) {
             j++;
         }
-        if (j == n) {
-            result.push_back(i - n +1 ); // fix indexing here
+
+        if (j == s2.size()) {
+            result.push_back(i - n + 1);
             j = pi[j-1];
+
+        }
+
+    }
+
+    // Add -1 for missing substrings
+
+    if (result.empty()) {
+        result.push_back(-1);
+
+    } else {
+        while (result.size() < n) {
+            result.push_back(-1);
         }
     }
+
     return result;
+
 }
--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 #include <vector>
 #include <chrono>
 #include "Finder.h"
+using namespace std;
 
 int main(){
+    vector<int> a;
+    vector<int> b;
 
+    Finder a;
+
+    
     return 0;
 }
