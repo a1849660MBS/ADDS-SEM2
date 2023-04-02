b788327f68930916c08b2f84134468a4a57444de
Marwan Salih <a1849660@adelaide.edu.au>
2023-04-02 20:10:27 +0930

    v12

--- Finder.cpp
+++ Finder.cpp
@@ -10,8 +10,8 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
     }
     // Create prefix table
     vector<int> pi(n);
-    for (int i = 1, j = 0; i < n; i++) {
-        while (j > 0 && s2[i] != s2[j]) {
+    for (int i = 0, j = 0; i < n; i++) {
+        while (j > 0 && (s2[i] != s2[j])) {
             j = pi[j-1];
         }
         if (s2[i] == s2[j]) {
@@ -21,14 +21,14 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
     }
     // Search for s2 in s1
     for (int i = 0, j = 0; i < m; i++) {
-        while (j > 0 && s1[i] != s2[j]) {
+        while (j > 0 && (s1[i] != s2[j])) {
             j = pi[j-1];
         }
         if (s1[i] == s2[j]) {
             j++;
         }
         if (j == n) {
-            result.push_back(i - n); // fix indexing here
+            result.push_back(i - n +1 ); // fix indexing here
             j = pi[j-1];
         }
     }
