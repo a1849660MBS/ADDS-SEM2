d0c1f619d0e9d034bfafbf7bcc7f1cca46754865
Marwan Salih <a1849660@adelaide.edu.au>
2023-04-02 19:36:16 +0930

    finder

--- Finder.cpp
+++ Finder.cpp
@@ -2,35 +2,45 @@
 using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
-    result.reserve(s2.size());
-    size_t s1_size = s1.size();
-    size_t s2_size = s2.size();
-    vector<int> pi(s2_size);
-    int j = 0;
-    for (int i = 1; i < s2_size; i++) {
-        while (j > 0 && s2[i] != s2[j]) {
-            j = pi[j-1];
-        }
-        if (s2[i] == s2[j]) {
-            j++;
-        }
-        pi[i] = j;
-    }
-    j = 0;
-    for (int i = 0; i < s1_size; i++) {
-        while (j > 0 && s1[i] != s2[j]) {
-            j = pi[j-1];
-        }
-        if (s1[i] == s2[j]) {
-            j++;
-        }
-        if (j == s2_size) {
-            result.emplace_back(i - j + 1);
-            j = pi[j-1];
+    int m = s1.size();
+    int n = s2.size();
+
+    // create LPS array for KMP algorithm
+    vector<int> lps(n, 0);
+    int len = 0;
+    int i = 1;
+    while (i < n) {
+        if (s2[i] == s2[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
         }
     }
-    if (result.empty()) {
-        result.emplace_back(-1);
+
+    // use sliding window approach with KMP algorithm
+    int i1 = 0, i2 = 0;
+    while (i1 < m) {
+        if (s1[i1] == s2[i2]) {
+            i1++;
+            i2++;
+        }
+        if (i2 == n) {
+            result.push_back(i1 - i2);
+            i2 = lps[i2 - 1];
+        } else if (i1 < m && s1[i1] != s2[i2]) {
+            if (i2 != 0) {
+                i2 = lps[i2 - 1];
+            } else {
+                i1++;
+            }
+        }
     }
     return result;
 }
