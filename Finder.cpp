adc8578b994f96a5bab1d77ff54144c97bd3a56d
Marwan Salih <a1849660@adelaide.edu.au>
2023-04-02 21:12:40 +0930

    v10000

--- Finder.cpp
+++ Finder.cpp
@@ -1,24 +1,17 @@
 #include "Finder.h"
 using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
-    vector<int> result;
-    size_t start = 0;
-    while (start < s1.size()) {
-        size_t found = s1.find(s2, start);
-        if (found != string::npos) {
-            result.push_back(found);
-            start = found + 1;
-        } else {
-            result.push_back(-1);
-            break;
-        }
-    }
-    // Check if all substrings have been found
-    if (result.back() == -1) {
-        int last_pos = result[result.size() - 2];
-        for (int i = last_pos + 1; i <= s1.size() - s2.size(); i++) {
-            result.push_back(-1);
-        }
-    }
-    return result;
+vector<int> result;
+size_t start = 0;
+while (start <= s1.size()) {
+size_t found = s1.find(s2, start);
+if (found != string::npos) {
+result.push_back(found);
+start = found + 1;
+} else {
+result.push_back(-1);
+break;
+}
+}
+return result;
 }
