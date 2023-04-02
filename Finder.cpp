2f3fc5616c95a83d8016ff4c71cf3329616b23f8
Marwan Salih <a1849660@adelaide.edu.au>
2023-04-02 21:20:00 +0930

    v2220000

--- Finder.cpp
+++ Finder.cpp
@@ -3,7 +3,7 @@ using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
 vector<int> result;
 size_t start = 0;
-while (start <= s1.size()) {
+while (start < s1.size()) {
 size_t found = s1.find(s2, start);
 
 if (found != string::npos) {
