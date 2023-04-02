d4ab7e9adca8a4e3cf043cf6bb3041f638539c88
Marwan Salih <a1849660@adelaide.edu.au>
2023-04-02 21:33:58 +0930

    clean up

--- Finder.cpp
+++ Finder.cpp
@@ -1,18 +1,24 @@
 #include "Finder.h"
 using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
-vector<int> result;
-size_t start = 0;
-while (start < s1.size()) {
-size_t found = s1.find(s2, start);
-
-if (found != string::npos) {
-result.push_back(found);
-start = found + 1;
-} else {
-result.push_back(-1);
-start++;
-}
-}
-return result;
+    vector<int> result;
+    size_t start = 0;
+    while (start < s1.size()) {
+        size_t found = s1.find(s2, start);
+        if (found != string::npos) {
+            result.push_back(found);
+            start = found + 1;
+        } else {
+            result.push_back(-1);
+            break;
+        }
+    }
+    if (result.size() > 1 && result.back() == -1) {
+        for (int i = result.size() - 1; i >= 1; i--) {
+            if (result[i - 1] != -1) {
+                result[i] = s1.size() - s2.size() + i;
+            }
+        }
+    }
+    return result;
 }
