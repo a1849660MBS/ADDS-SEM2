8fb85d5c070258813e610745783e6bb725317a24
Marwan Salih <a1849660@adelaide.edu.au>
2023-04-02 19:47:19 +0930

    finder v6

--- Finder.cpp
+++ Finder.cpp
@@ -1,45 +1,34 @@
 #include "Finder.h"
+
 using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
+
     vector<int> result;
-    int m = s1.size();
-    int n = s2.size();
 
-    if (n > m) {
-        return result;
-    }
+    result.reserve(s2.size());
 
-    const int p = 31;  // prime number for hash function
-    const int mod = 1e9 + 9;  // mod for hash function
+    size_t s2_size = s2.size();
 
-    // Calculate hash values for s2 and for each substring of s1 of length n
-    long long hash_s2 = 0;
-    long long hash_s1 = 0;
-    long long p_pow = 1;
-    for (int i = 0; i < n; i++) {
-        hash_s2 = (hash_s2 + (s2[i] - 'a' + 1) * p_pow) % mod;
-        hash_s1 = (hash_s1 + (s1[i] - 'a' + 1) * p_pow) % mod;
-        p_pow = (p_pow * p) % mod;
-    }
+    string s2_substr;
 
-    for (int i = 0; i <= m - n; i++) {
-        if (hash_s1 == hash_s2) {
-            bool found = true;
-            for (int j = 0; j < n; j++) {
-                if (s1[i + j] != s2[j]) {
-                    found = false;
-                    break;
-                }
-            }
-            if (found) {
-                result.push_back(i);
-            }
+    for (int i = 0; i <= s2_size; i++) {
+
+        s2_substr = s2.substr(0, i);
+
+        size_t found = s1.find(s2_substr);
+
+        if (found != string::npos) {
+
+            result.emplace_back(found);
+
+        } else {
+
+            result.emplace_back(-1);
+
         }
-        if (i < m - n) {
-            hash_s1 = (hash_s1 - (s1[i] - 'a' + 1) * p_pow + mod) % mod;
-            hash_s1 = (hash_s1 * p + s1[i + n] - 'a' + 1) % mod;
-        }
+
     }
 
     return result;
+
 }
