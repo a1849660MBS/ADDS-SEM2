074052519220bb810298cf3e6a3e9e0b03b9452e
Marwan Salih <a1849660@adelaide.edu.au>
2023-04-02 19:27:42 +0930

    branch publish

---
+++ Finder.cpp
@@ -0,0 +1,18 @@
+#include "Finder.h"
+using namespace std;
+vector<int> Finder::findSubstrings(string s1, string s2) {
+    vector<int> result;
+    result.reserve(s2.size());
+    size_t s2_size = s2.size();
+    string s2_substr;
+    for (size_t i = 1; i <= s2_size; i++) {
+        s2_substr = s2.substr(0, i);
+        size_t found = s1.find(s2_substr);
+        if (found != string::npos) {
+            result.emplace_back(found);
+        } else {
+            result.emplace_back(-1);
+        }
+    }
+    return result;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include "Finder.h"
+
+int main(){
+
+    return 0;
+}
