bb40cfa09a225e09d41577212fe76f6c0c458ca9
Marwan Salih <a1849660@adelaide.edu.au>
2023-04-02 19:42:28 +0930

    finder v4

--- Finder.cpp
+++ Finder.cpp
@@ -1,46 +1,43 @@
-#include "Finder.h"
-using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
     int m = s1.size();
     int n = s2.size();
 
-    // create LPS array for KMP algorithm
-    vector<int> lps(n, 0);
-    int len = 0;
-    int i = 1;
-    while (i < n) {
-        if (s2[i] == s2[len]) {
-            len++;
-            lps[i] = len;
-            i++;
-        } else {
-            if (len != 0) {
-                len = lps[len - 1];
-            } else {
-                lps[i] = 0;
-                i++;
+    if (n > m) {
+        return result;
+    }
+
+    const int p = 31;  // prime number for hash function
+    const int mod = 1e9 + 9;  // mod for hash function
+
+    // Calculate hash values for s2 and for each substring of s1 of length n
+    long long hash_s2 = 0;
+    long long hash_s1 = 0;
+    long long p_pow = 1;
+    for (int i = 0; i < n; i++) {
+        hash_s2 = (hash_s2 + (s2[i] - 'a' + 1) * p_pow) % mod;
+        hash_s1 = (hash_s1 + (s1[i] - 'a' + 1) * p_pow) % mod;
+        p_pow = (p_pow * p) % mod;
+    }
+
+    for (int i = 0; i <= m - n; i++) {
+        if (hash_s1 == hash_s2) {
+            bool found = true;
+            for (int j = 0; j < n; j++) {
+                if (s1[i + j] != s2[j]) {
+                    found = false;
+                    break;
+                }
             }
+            if (found) {
+                result.push_back(i);
+            }
+        }
+        if (i < m - n) {
+            hash_s1 = (hash_s1 - (s1[i] - 'a' + 1) * p_pow + mod) % mod;
+            hash_s1 = (hash_s1 * p + s1[i + n] - 'a' + 1) % mod;
         }
     }
 
-    // use sliding window approach with KMP algorithm
-    int i1 = 0, i2 = 0;
-    while (i1 < m) {
-        if (s1[i1] == s2[i2]) {
-            i1++;
-            i2++;
-        }
-        if (i2 == n) {
-            result.push_back(i1 - i2);
-            i2 = lps[i2 - 1];
-        } else if (i1 < m && s1[i1] != s2[i2]) {
-            if (i2 != 0) {
-                i2 = lps[i2 - 1];
-            } else {
-                i1++;
-            }
-        }
-    }
     return result;
 }
