91f92d329c4915266d54cd2bab64efe8aced4703
Marwan Salih <a1849660@adelaide.edu.au>
2023-05-14 18:47:05 +0930

    fixes

---
+++ Autocomplete.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+
+
+const int MAX_NODE = 26;
+
+class TrieNode {
+public:
+    std::vector<TrieNode *> children;
+    bool isEndOfWord;
+    
+    TrieNode();
+};
+
+class Autocomplete {
+private:
+    TrieNode* root;
+    
+    void insertHelper(TrieNode* node, std::string word);
+    
+    void getSuggestionsHelper(TrieNode* node, std::string partialWord, std::vector<string>& suggestions);
+    
+    
+public:
+    Autocomplete();
+    
+    void insert(std::string word);
+    
+    std::vector<std::string> getSuggestions(std::string partialWord);
+};
--- autocomplete.cpp
+++ autocomplete.cpp
@@ -1,27 +1,15 @@
+#include <iostream>
 #include <vector>
 #include <string>
-#include <iostream>
+#include "Autocomplete.h"
+ using namespace std;
 
-using namespace std;
-
-const int MAX_NODE = 26;
-
-class TrieNode {
-public:
-    vector<TrieNode*> children;
-    bool isEndOfWord;
-    
-    TrieNode() {
-        children = vector<TrieNode*>(MAX_NODE, nullptr);
+    TrieNode::TrieNode() {
+        children =  std::vector<TrieNode*>(MAX_NODE, nullptr);
         isEndOfWord = false;
     }
-};
 
-class Autocomplete {
-private:
-    TrieNode* root;
-    
-    void insertHelper(TrieNode* node, string word) {
+        void Autocomplete::insertHelper(TrieNode* node, string word) {
         for (char c : word) {
             int index = c - 'a';
             if (node->children[index] == nullptr) {
@@ -31,8 +19,8 @@ private:
         }
         node->isEndOfWord = true;
     }
-    
-    void getSuggestionsHelper(TrieNode* node, string partialWord, vector<string>& suggestions) {
+
+        void Autocomplete::getSuggestionsHelper(TrieNode* node, string partialWord, vector<string>& suggestions) {
         if (node->isEndOfWord) {
             suggestions.push_back(partialWord);
         }
@@ -42,17 +30,17 @@ private:
             }
         }
     }
-    
-public:
-    Autocomplete() {
+
+
+        Autocomplete::Autocomplete() {
         root = new TrieNode();
     }
-    
-    void insert(string word) {
+
+        void Autocomplete::insert(string word) {
         insertHelper(root, word);
     }
-    
-    vector<string> getSuggestions(string partialWord) {
+
+    vector<string> Autocomplete::getSuggestions(string partialWord) {
         vector<string> suggestions;
         TrieNode* node = root;
         for (char c : partialWord) {
@@ -65,39 +53,3 @@ public:
         getSuggestionsHelper(node, partialWord, suggestions);
         return suggestions;
     }
-};
-
-
-
-int main() {
-    Autocomplete autocomplete;
-    autocomplete.insert("apple");
-    autocomplete.insert("banana");
-    autocomplete.insert("orange");
-    
-    vector<string> suggestions = autocomplete.getSuggestions("a");
-    for (string suggestion : suggestions) {
-        cout << suggestion << endl;
-    }
-    // Output: "apple"
-    
-    suggestions = autocomplete.getSuggestions("b");
-    for (string suggestion : suggestions) {
-        cout << suggestion << endl;
-    }
-    // Output: "banana"
-    
-    suggestions = autocomplete.getSuggestions("o");
-    for (string suggestion : suggestions) {
-        cout << suggestion << endl;
-    }
-    // Output: "orange"
-    
-    suggestions = autocomplete.getSuggestions("ap");
-    for (string suggestion : suggestions) {
-        cout << suggestion << endl;
-    }
-    // Output: "apple"
-    
-    return 0;
-}
